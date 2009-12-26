ddc1ee1092567dd04a43c16cf5e875a8de8800cd
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-12-26 18:53:20 +0900

    initial import

---
+++ lib/HTML/EFT.pm
@@ -0,0 +1,35 @@
+package HTML::EFT;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+HTML::EFT -
+
+=head1 SYNOPSIS
+
+  use HTML::EFT;
+
+=head1 DESCRIPTION
+
+HTML::EFT is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
