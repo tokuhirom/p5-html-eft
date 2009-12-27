3bc07d8f3191e7102a7f3dcdfb160eeda05bd970
tokuhirom <tokuhirom@gmail.com>
2009-12-27 15:09:43 +0900

    fixed for http://www.sugoren.com/report/post_503.php

--- lib/HTML/EFT/GoogleAdSection.pm
+++ lib/HTML/EFT/GoogleAdSection.pm
@@ -6,10 +6,14 @@ sub new { bless {}, shift }
 
 sub extract {
     my ($self, $url, $html) = @_;
-    if ($html =~ m{<!--\s+google_ad_section_start\s+-->(.+)<!--\s+google_ad_section_end\s+-->}) {
-        return $1;
+    my @ret;
+    while ($html =~ m{<!--\s+google_ad_section_start\s+-->(.+?)<!--\s+google_ad_section_end\s+-->}gs) {
+        push @ret, $1;
+    }
+    if (@ret) {
+        return join "\n", @ret;
     } else {
-        return; # not matched!
+        return; # not matched
     }
 }
 
--- t/100_child/01_google_adsection.t
+++ t/100_child/01_google_adsection.t
@@ -6,5 +6,5 @@ use Test::More tests => 1;
 my $eft = HTML::EFT->new(
     'GoogleAdSection'
 );
-my $ret = $eft->extract('http://example.com/', q{ppp<!-- google_ad_section_start -->hoge<!-- google_ad_section_end -->qqq});
-is $ret, 'hoge';
+my $ret = $eft->extract('http://example.com/', q{ppp<!-- google_ad_section_start -->hoge<!-- google_ad_section_end --><!-- google_ad_section_start -->fuga<!-- google_ad_section_end -->qqq});
+is $ret, "hoge\nfuga";
